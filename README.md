410f09bfd759a2b90dedee477038db97b4276c93
emreby <emreby@gmail.com>
2020-11-23 15:10:20 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Precise Ultrasonic Sensor
+This is a small script to calibrate ultrasonic sensor based on the environmental conditions, i.e. temperature and humidity.
+As the speed of sound depends on these conditions, this small script calibrates default ultrasonic sensor distance calculation.
+Both sensors as well as the base code is from Sunfounder
---
+++ ultrasonicPrecise.py
@@ -0,0 +1,211 @@
+'''
+**********************************************************************
+* Filename    : dht11.py
+* Description : test for SunFoudner DHT11 humiture & temperature module
+* Author      : Dream
+* Brand       : SunFounder
+* E-mail      : service@sunfounder.com
+* Website     : www.sunfounder.com
+* Update      : Dream    2016-09-30    New release
+**********************************************************************
+'''
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import time
+
+DHTPIN = 15
+TRIG = 11
+ECHO = 12
+
+GPIO.setmode(GPIO.BOARD)
+
+def setup():
+	GPIO.setup(TRIG, GPIO.OUT)
+	GPIO.setup(ECHO, GPIO.IN)
+
+MAX_UNCHANGE_COUNT = 100
+
+STATE_INIT_PULL_DOWN = 1
+STATE_INIT_PULL_UP = 2
+STATE_DATA_FIRST_PULL_DOWN = 3
+STATE_DATA_PULL_UP = 4
+STATE_DATA_PULL_DOWN = 5
+
+def read_dht11_dat():
+	GPIO.setup(DHTPIN, GPIO.OUT)
+	GPIO.output(DHTPIN, GPIO.HIGH)
+	time.sleep(0.05)
+	GPIO.output(DHTPIN, GPIO.LOW)
+	time.sleep(0.02)
+	GPIO.setup(DHTPIN, GPIO.IN, GPIO.PUD_UP)
+
+	unchanged_count = 0
+	last = -1
+	data = []
+	while True:
+		current = GPIO.input(DHTPIN)
+		data.append(current)
+		if last != current:
+			unchanged_count = 0
+			last = current
+		else:
+			unchanged_count += 1
+			if unchanged_count > MAX_UNCHANGE_COUNT:
+				break
+
+	state = STATE_INIT_PULL_DOWN
+
+	lengths = []
+	current_length = 0
+
+	for current in data:
+		current_length += 1
+
+		if state == STATE_INIT_PULL_DOWN:
+			if current == GPIO.LOW:
+				state = STATE_INIT_PULL_UP
+			else:
+				continue
+		if state == STATE_INIT_PULL_UP:
+			if current == GPIO.HIGH:
+				state = STATE_DATA_FIRST_PULL_DOWN
+			else:
+				continue
+		if state == STATE_DATA_FIRST_PULL_DOWN:
+			if current == GPIO.LOW:
+				state = STATE_DATA_PULL_UP
+			else:
+				continue
+		if state == STATE_DATA_PULL_UP:
+			if current == GPIO.HIGH:
+				current_length = 0
+				state = STATE_DATA_PULL_DOWN
+			else:
+				continue
+		if state == STATE_DATA_PULL_DOWN:
+			if current == GPIO.LOW:
+				lengths.append(current_length)
+				state = STATE_DATA_PULL_UP
+			else:
+				continue
+	if len(lengths) != 40:
+		#print ("Data not good, skip")
+		return False
+
+	shortest_pull_up = min(lengths)
+	longest_pull_up = max(lengths)
+	halfway = (longest_pull_up + shortest_pull_up) / 2
+	bits = []
+	the_bytes = []
+	byte = 0
+
+	for length in lengths:
+		bit = 0
+		if length > halfway:
+			bit = 1
+		bits.append(bit)
+	#print ("bits: %s, length: %d" % (bits, len(bits)))
+	for i in range(0, len(bits)):
+		byte = byte << 1
+		if (bits[i]):
+			byte = byte | 1
+		else:
+			byte = byte | 0
+		if ((i + 1) % 8 == 0):
+			the_bytes.append(byte)
+			byte = 0
+	#print (the_bytes)
+	checksum = (the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3]) & 0xFF
+	if the_bytes[4] != checksum:
+		#print ("Data not good, skip")
+		return False
+
+	return the_bytes[0], the_bytes[2]
+
+def distance(humidityM, temperatureM):
+	GPIO.output(TRIG, 0)
+	time.sleep(0.000002)
+
+	GPIO.output(TRIG, 1)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, 0)
+
+	
+	while GPIO.input(ECHO) == 0:
+		a = 0
+	time1 = time.time()
+	while GPIO.input(ECHO) == 1:
+		a = 1
+	time2 = time.time()
+
+	during = time2 - time1
+	
+# https://www.engineeringtoolbox.com/air-speed-sound-d_603.html
+	
+	humFactor = ((humidityM-25)/100)*1.0
+	Vms = (20.05*((273.16 + temperatureM)**0.5)) + humFactor
+	
+	print("Calculated Speed of Sound is: %s " % Vms)
+	
+	return during * 340 / 2 * 100
+
+def main():
+	print ("Raspberry Pi wiringPi DHT11 Temperature test program\n")
+	
+	humArray = []
+	tempArray = []
+	i = 0
+	
+	while True:
+		result = read_dht11_dat()
+		if result:
+			humidity, temperature = result
+			print ("Preprocessing humidity: %s %%,  Temperature: %s C`" % (humidity, temperature))
+			humArray.append(humidity)
+			tempArray.append(temperature)
+			i += 1
+			print(i)
+		time.sleep(1)
+		
+		if len(humArray) == 20:
+			print("Preprocessing done")
+			humAvr = sum(humArray) / len(humArray)
+			tempAvr = sum(tempArray) / len(tempArray)
+			break
+	
+	with open('humValues.txt', 'w') as f:
+		for item in humArray:
+			f.write("%s\n" % item)
+		f.close()		
+#	print("Humidity values are written")
+	
+	with open('tempValues.txt', 'w') as f:
+		for item in tempArray:
+			f.write("%s\n" % item)
+		f.close()
+#    print("Temperature values are written")
+	
+	while True:
+		# for humidity
+		'''
+		result = read_dht11_dat()
+		if result:
+			humidity, temperature = result
+			print ("humidity: %s %%,  Temperature: %s C`" % (humidity, temperature))
+		'''
+		# for ultrasound
+		dis = distance(humAvr, tempAvr)
+		print (dis, 'cm')
+		print ('')
+		
+		time.sleep(0.5)
+
+def destroy():
+	GPIO.cleanup()
+
+if __name__ == "__main__":
+	setup()
+	try:
+		main()
+	except KeyboardInterrupt:
+		destroy()
